66d9845ab57405101c33800536866695be7cd164
José Kleyton <106536950+kl3yton@users.noreply.github.com>
2024-11-04 22:15:49 -0300

    1.2 Commit projeto_php
    Foi excluído o arquivo header e atualizado o index e bd_agenda.

--- index.php
+++ index.php
@@ -1,37 +1,28 @@
 <?php 
-    include ('header.php');
-?>
-<?php 
     include ('bd_agenda.php');
 ?>
 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seistema de Agendas</title>
+</head>
+<body>
+    <header>
+        <h1>Sistema Agendador</h1>
+        
+        <nav>
+        <a href="">Home</a>
+        <a href="">Contato</a>
+        <a href="">Tarefas</a>
+        <a href="">Eventos</a>
+        <a href="">Favoritos</a>
+        </nav>
+    </header>
+    <main>
 
-<div>
-    <h2>contatos Cadastrados</h2>
-    <table>
-        <thead>
-            <tr>
-                <th>Títulos</th>
-                <th>Descrição</th>
-                <th>Ano</th>
-                <th>Preço</th>
-                <th>Disponível</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-                if(false) {
-
-                }else{
-                    echo "<p>Nenhum filme cadastrado.</p>";
-                }
-            ?>
-        </tbody>
-    </table>
-</div>
-
-
-<?php 
-    include('footer.php');
-?>
+    </main>
+</body>
+</html>
