52cd09122727ab51f2697876d038ce004e3c3927
José Kleyton <106536950+kl3yton@users.noreply.github.com>
2024-11-07 07:43:37 -0300

    Atualizando

---
+++ db/db_agenda.php
@@ -0,0 +1,40 @@
+<?php 
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$dbname = 'agenda_telefonica';
+
+$conexao = new mysqli($host, $user, $password);
+
+// Verifica a conexão
+if ($conexao->connect_error) {
+    die("Falha na conexão: " . $conexao->connect_error);
+}
+
+// Cria o banco de dados se não existir
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+
+if ($conexao->query($sql) === TRUE) {
+    $conexao->select_db($dbname);
+    
+    // Cria a tabela se não existir
+    $sql = "CREATE TABLE IF NOT EXISTS tb_contato(
+        idcontato INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        nomeContato VARCHAR(200) NOT NULL,
+        emailContato VARCHAR(100) NOT NULL,
+        telefoneContato VARCHAR(50) NOT NULL,
+        data_nasc_Contato DATE NOT NULL,
+        tagFavorito TINYINT(1) DEFAULT NULL
+    )";
+
+    if ($conexao->query($sql) === FALSE) {
+        die("Erro na criação da tabela: " . $conexao->error);
+    } 
+    // A linha de sucesso foi removida
+} else {
+    die("Erro ao criar banco de dados: " . $conexao->error);
+}
+
+$conexao->close();
+?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-    include ('bd_agenda.php');
+    include ("db/db_agenda.php");
 ?>
 
 <!DOCTYPE html>
@@ -7,22 +7,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Seistema de Agendas</title>
+    <title>Sistema de Agendas</title>
 </head>
 <body>
     <header>
         <h1>Sistema Agendador</h1>
         
-        <nav>
-        <a href="">Home</a>
-        <a href="">Contato</a>
-        <a href="">Tarefas</a>
-        <a href="">Eventos</a>
-        <a href="">Favoritos</a>
+        <nav> <!-- tag menuop é usada para url -->
+            <a href="index.php?menuop=home">Home</a>
+            <a href="index.php?menuop=contatos">Contatos</a>
+            <a href="index.php?menuop=tarefas">Tarefas</a>
+            <a href="index.php?menuop=eventos">Eventos</a>
         </nav>
     </header>
     <main>
-
+        <?php //Quando clicar no link e ele é encaminhado para um local do site.
+            $menuop = (isset($_GET["menuop"]))?$_GET["menuop"]:"home";
+            switch ($menuop) {
+                case 'home':
+                    include("paginas/home/home.php");
+                    break;
+                case 'contatos':
+                    include("paginas/contatos/contatos.php");
+                    break;
+                case 'tarefas':
+                    include("paginas/tarefas/tarefas.php");
+                    break;
+                case 'eventos':
+                    include("paginas/eventos/eventos.php");
+                    break;
+                default:
+                    include("paginas/home/home.php");
+                        break;
+            }
+        ?>
     </main>
 </body>
 </html>
