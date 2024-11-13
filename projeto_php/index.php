681fd7925454b144bc522c811c8dcdd07253cdc4
devdiiego <156592819+devdiiego@users.noreply.github.com>
2024-11-13 12:10:33 -0300

    Add files via upload

--- projeto_php/index.php
+++ projeto_php/index.php
@@ -7,7 +7,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--link CDN pego do bootstrap aula-11 -->
+
+    <!-- Estilização do site / bootstrap aula-11 --> <!-- Estilização do site / bootstrap aula-11 --> 
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
@@ -15,54 +17,72 @@
     <title>Sistema de Agendas</title>
 </head>
 <body>
-    <header>
-        <h1>Sistema Agendador 1.0</h1>
-        
-        <nav> <!-- tag menuop é usada para url -->
-            <a href="index.php?menuop=home">Home</a>
-            <a href="index.php?menuop=contatos">Contatos</a>
-            <a href="index.php?menuop=tarefas">Tarefas</a>
-            <a href="index.php?menuop=eventos">Eventos</a>
+   <header class="bg-dark">
+    <div class="container">
+       
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo_agendador.png" alt="Sis-Agendador" width="120">
+            </a>
+
+                    <!-- Estilização dos links do navegador utilizadno as navbar do bootsrap-->
+
+            <div class="collapse navbar-collapse" id="ConteudoNavbarSuportado">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item" id="home"><a class="nav-link" href="index.php?menuop=home">Home</a></li>
+                    <li class="nav-item" id="contatos"><a class="nav-link" href="index.php?menuop=contatos">Contatos</a></li>
+                    <li class="nav-item" id="tarefas"><a class="nav-link" href="index.php?menuop=tarefas">Tarefas</a></li>
+                    <li class="nav-item" id="eventos"><a class="nav-link" href="index.php?menuop=eventos">Eventos</a></li>
+                </ul>
+            </div>
         </nav>
-    </header> 
-    <hr>
+    </div>
+</header>
+
     <main>
-        <?php //Quando clicar no link e ele é encaminhado para um local do site.
-            $menuop = (isset($_GET["menuop"]))?$_GET["menuop"]:"home";
-            switch ($menuop) {
-                case 'home':
-                    include("paginas/home/home.php");
+    <div class="container">
+
+    <?php
+        $menuop = (isset($_GET["menuop"]))?$_GET["menuop"]:"home";
+        switch ($menuop) {
+            case 'home':
+                include("paginas/home/home.php");
+                break;
+            case 'contatos':
+                include("paginas/contatos/contatos.php");
+                break;
+            case 'cad-contatos':
+                include("paginas/contatos/cad-contato.php");
+                break;
+            case 'inserir-contato':
+                include("paginas/contatos/inserir-contato.php");
+                break;
+            case 'editar-contato':
+                    include("paginas/contatos/editar-contato.php");
+                break;
+            case 'excluir-contato':
+                    include("paginas/contatos/excluir-contato.php");
+                break;
+            case 'atualizar-contato':
+                    include("paginas/contatos/atualizar-contato.php");
                     break;
-                case 'contatos':
-                    include("paginas/contatos/contatos.php");
+            case 'tarefas':
+                include("paginas/tarefas/tarefas.php");
+                break;
+            case 'eventos':
+                include("paginas/eventos/eventos.php");
+                break;
+            default:
+                include("paginas/home/home.php");
                     break;
-                case 'cad-contatos':
-                    include("paginas/contatos/cad-contato.php");
-                    break;
-                case 'inserir-contato':
-                    include("paginas/contatos/inserir-contato.php");
-                    break;
-                case 'editar-contato':
-                        include("paginas/contatos/editar-contato.php");
-                    break;
-                case 'excluir-contato':
-                        include("paginas/contatos/excluir-contato.php");
-                    break;
-                case 'atualizar-contato':
-                        include("paginas/contatos/atualizar-contato.php");
-                        break;
-                case 'tarefas':
-                    include("paginas/tarefas/tarefas.php");
-                    break;
-                case 'eventos':
-                    include("paginas/eventos/eventos.php");
-                    break;
-                default:
-                    include("paginas/home/home.php");
-                        break;
-            }
-        ?>
+        }
+    ?>
+    </div>
     </main>
+    <footer class="container-fluid fixed-bottom bg-dark">
+        <div class="text-center">Sistema Agendador V 1.0</div>
+
+    </footer>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 </body>
