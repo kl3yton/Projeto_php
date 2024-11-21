807068cb79b704a009b942c3aa9849e1a697ad75
José Kleyton <106536950+kl3yton@users.noreply.github.com>
2024-11-21 08:31:34 -0300

    Atualizção

---
+++ Projeto_php/index.php
@@ -0,0 +1,91 @@
+<?php 
+    include ("db/db_agenda.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Estilização do site / bootstrap aula-11 --> <!-- Estilização do site / bootstrap aula-11 --> 
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    
+    <link rel="stylesheet" href="css/estilo-padrao.css">
+    <title>Sistema de Agendas</title>
+</head>
+<body>
+   <header class="bg-dark">
+    <div class="container">
+       
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php?menuop=home">
+                <img src="img/logo_agendador.png" alt="Sis-Agendador" width="120">
+            </a>
+
+                    <!-- Estilização dos links do navegador utilizadno as navbar do bootsrap-->
+
+            <div class="collapse navbar-collapse" id="ConteudoNavbarSuportado">
+                <ul class="navbar-nav mr-auto">
+
+                    <li class="nav-item active" id="home"><a class="nav-link" href="index.php?menuop=home">Home</a></li>
+
+                    <li class="nav-item" id="contatos"><a class="nav-link" href="index.php?menuop=contatos">Contatos</a></li>
+
+                    <li class="nav-item" id="tarefas"><a class="nav-link" href="index.php?menuop=tarefas">Tarefas</a></li>
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
+
+    <main>
+    <div class="container">
+
+    <?php
+        $menuop = (isset($_GET["menuop"]))?$_GET["menuop"]:"home";
+        switch ($menuop) {
+            case 'home':
+                include("paginas/home/home.php");
+                break;
+            case 'contatos':
+                include("paginas/contatos/contatos.php");
+                break;
+            case 'cad-contatos':
+                include("paginas/contatos/cad-contato.php");
+                break;
+            case 'inserir-contato':
+                include("paginas/contatos/inserir-contato.php");
+                break;
+            case 'editar-contato':
+                    include("paginas/contatos/editar-contato.php");
+                break;
+            case 'excluir-contato':
+                    include("paginas/contatos/excluir-contato.php");
+                break;
+            case 'atualizar-contato':
+                    include("paginas/contatos/atualizar-contato.php");
+                    break;
+            case 'tarefas':
+                include("paginas/tarefas/tarefas.php");
+                break;
+            default:
+                include("paginas/home/home.php");
+                    break;
+        }
+    ?>
+    </div>
+    </main>
+    <footer class="container-fluid bg-dark">
+        <div class="text-center">Sistema Agendador V 1.0</div>
+
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="./js/validation.js"></script>
+</body>
+</html>
---
+++ Projeto_php/paginas/contatos/contatos.php
@@ -0,0 +1,128 @@
+<?php 
+    include ("db/db_agenda.php");
+
+    //Váriavel da pesquisa
+    $txt_pesquisa = isset($_POST["txt_pesquisa"]) ? $_POST["txt_pesquisa"] : "";
+
+?>
+<head>
+    <h3><i class="bi bi-person-square"> </i>Contatos</h3>
+</head>
+<div>
+    <a class="btn btn-outline-primary mb-2" href="index.php?menuop=cad-contatos"><i class="bi bi-person-fill-add"></i> Novo Contato</a>
+</div>
+<div>
+    <form action="index.php?menuop=contatos" method="post">
+        <div class="input-group">
+        <input class="form-control col-3" type="text" name="txt_pesquisa" value="<?=$txt_pesquisa?>">
+        <button class="btn btn-outline-success btn-sm" type="submit"> <i class="bi bi-search"> </i>Pesquisa</button>
+        </div>
+    </form>
+</div>
+<div class="table1">
+    <table class="table table-dark table-hover table-bordered table-sm">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Telefone</th>
+                <th>E-mail</th>
+                <th>Endereço</th>
+                <th>Data de Nasc.</th>
+                <th>Editar</th>
+                <th>Excluir</th>
+                
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            // paginação aula-10
+            $quantidade = 10;
+            
+            $pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+
+            $inicio = ($pagina - 1) * $quantidade;
+
+            $sql = "SELECT 
+                        idContato,
+                        upper(nomeContato) AS nomeContato,
+                        lower(emailContato) AS emailContato,
+                        telefoneContato,
+                        upper(enderecoContato) AS enderecoContato,
+                        date_format(data_nasc_Contato, '%d/%m/%y') AS data_nasc_Contato
+                    FROM agenda_telefonica.tb_contato
+                    WHERE 
+                        idContato = '{$txt_pesquisa}' OR
+                        nomeContato LIKE '%{$txt_pesquisa}%'
+                    ORDER BY idContato ASC  
+                    LIMIT $inicio, $quantidade";  
+
+            $rs = mysqli_query($conexao, $sql) or die("Erro ao conectar a consulta: " . mysqli_error($conexao));
+
+            while ($dados = mysqli_fetch_assoc($rs)) {
+        ?>
+            <tr>
+                <td><?=$dados["idContato"] ?></td>
+                <td class="text-nowrap"><?=$dados["nomeContato"] ?></td>
+                <td class="text-nowrap"><?=$dados["telefoneContato"] ?></td>
+                <td class="text-nowrap"><?=$dados["emailContato"] ?></td>
+                <td class="text-nowrap"><?=$dados["enderecoContato"] ?></td>
+                <td><?=$dados["data_nasc_Contato"] ?></td>
+                <td class="text-center">
+                    <a class="btn btn-outline-warning btn-sm" href="index.php?menuop=editar-contato&idContato=<?=$dados["idContato"] ?>"><i class="bi bi-pencil-square"></i></a>
+                </td>
+                <td class="text-center">
+                    <a class="btn btn-outline-danger btn-sm" href="index.php?menuop=excluir-contato&idContato=<?=$dados["idContato"] ?>"><i class="bi bi-trash"></i></a>
+                </td>
+            </tr>
+    <?php 
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
+<br>
+
+<ul class="pagination justify-content-center">
+
+
+<?php 
+    // contar o total de registros
+    $sqlTotal = "SELECT idContato FROM tb_contato";
+    $qrTotal = mysqli_query($conexao, $sqlTotal) or die(mysqli_error($conexao));
+    $numTotal = mysqli_num_rows($qrTotal);
+
+    $totalPagina = ceil($numTotal / $quantidade);
+    
+    echo '<li class="page-item"><span class="page-link">Total de registros: ' . $numTotal . '</span></li>';
+
+    echo '<li class="page-item"><a class="page-link" href="?menuop=contatos&pagina=1">Primeira Página</a></li>';
+
+    if ($pagina > 1) {
+        ?>
+        <li class="page-item"><a class="page-link" href="?menuop=contatos&pagina=<?php echo $pagina - 1; ?>"> << </a></li>
+        <?php
+    }
+
+    for ($i = 1; $i <= $totalPagina; $i++) {
+        if($i>=($pagina-5) && $i <= ($pagina+5)){
+            if ($i == $pagina) {
+                echo "<li class='page-item active'><span class='page-link'>$i</span></li>";
+            } else {
+                echo "<li clas='page-item'><a class='page-link'href=\"?menuop=contatos&pagina=$i\">$i</a></li>";
+            }
+        }
+    }
+
+    if ($pagina < $totalPagina) {
+        ?>
+        <li class="page-item"><a class="page-link" href="?menuop=contatos&pagina=<?php echo $pagina + 1; ?>"> >> </a></li>
+        <?php
+    }
+
+    echo "<li class='page-item'><a class='page-link' href=\"?menuop=contatos&pagina=$totalPagina\">Última Página</a></li>";
+?>
+</ul>
+
+
+
